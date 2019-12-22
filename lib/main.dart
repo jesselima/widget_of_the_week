5edf6ae28c137ab3f5e12803cbe6d34e52408169
jesselima <jesselima.tech@gmail.com>
2019-12-21 22:32:07 -0300

    Improved: FloatingActionButton and BottomAppBar with Notch

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,6 @@ import 'package:widget_of_the_week/my_card.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -18,7 +17,6 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatefulWidget {
-
   final String title;
 
   MyHomePage({Key key, this.title}) : super(key: key);
@@ -29,28 +27,34 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
 
-  int _selectedIndex = 0;
-  static const TextStyle optionStyle = TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
-  static const List<Widget> _widgetOptions = <Widget>[
-    Text(
-      'Index 0: Home',
-      style: optionStyle,
-    ),
-    Text(
-      'Index 1: Business',
-      style: optionStyle,
-    ),
-    Text(
-      'Index 2: School',
-      style: optionStyle,
-    ),
-  ];
+  // ignore: slash_for_doc_comments
+  /**
+   * SAMPLE BOTTOM NAV MENU - TRADITIONAL
+   */
 
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
+//  int _selectedIndex = 0;
+//  static const TextStyle optionStyle =
+//      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+//  static const List<Widget> _widgetOptions = <Widget>[
+//    Text(
+//      'Index 0: Home',
+//      style: optionStyle,
+//    ),
+//    Text(
+//      'Index 1: Business',
+//      style: optionStyle,
+//    ),
+//    Text(
+//      'Index 2: School',
+//      style: optionStyle,
+//    ),
+//  ];
+//
+//  void _onItemTapped(int index) {
+//    setState(() {
+//      _selectedIndex = index;
+//    });
+//  }
 
 //  @override
 //  Widget build(BuildContext context) {
@@ -83,15 +87,20 @@ class _MyHomePageState extends State<MyHomePage> {
 //    );
 //  }
 
+
+  // ignore: slash_for_doc_comments
+  /**
+   * SAMPLE WITH NOTCH
+   */
+
   Widget build(BuildContext context) {
     return new Scaffold(
-
       appBar: AppBar(title: const Text('Bottom App Bar')),
-
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
       floatingActionButton: FloatingActionButton(
-        child: const Icon(Icons.add), onPressed: () {},),
-
+        child: const Icon(Icons.add),
+        onPressed: () {},
+      ),
       bottomNavigationBar: BottomAppBar(
         shape: CircularNotchedRectangle(),
         color: Colors.blueGrey,
@@ -100,18 +109,34 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisSize: MainAxisSize.max,
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: <Widget>[
-            IconButton(icon: Icon(Icons.menu), onPressed: () {},),
-            IconButton(icon: Icon(Icons.wifi), onPressed: () {},),
-            IconButton(icon: Icon(Icons.person), onPressed: () {},),
-            IconButton(icon: Icon(Icons.search), onPressed: () {},),
+            /**
+             * Try ro comment and uncomment lines
+             * Use "flex" argument when you want to change the balance between
+             * ...the Expanded. Like LinearLayout Weight in Android xml layouts.
+             */
+            Expanded(
+                flex: 0,
+                child: IconButton(icon: Icon(Icons.menu), onPressed: () {})),
+            Expanded(
+                flex: 2,
+                child: IconButton(icon: Icon(Icons.wifi), onPressed: () {})),
+
+            Expanded(
+                flex: 1,
+                child: SizedBox(width: 20,)
+            ),
+
+            Expanded(
+                flex: 2,
+                child: IconButton(icon: Icon(Icons.security), onPressed: () {})),
+            Expanded(
+                flex: 0,
+                child: IconButton(icon: Icon(Icons.search), onPressed: () {})),
+
           ],
         ),
       ),
     );
   }
 
-
-
 }
-
-
