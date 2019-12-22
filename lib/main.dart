9f229ae7faf662779db25f1777377821a103e8f4
jesselima <jesselima.tech@gmail.com>
2019-12-22 07:14:57 -0300

    SilverBar and NestedScrollView Sample 1

--- lib/main.dart
+++ lib/main.dart
@@ -32,17 +32,32 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("SafeArea"),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            MyCard(cardColor: Colors.amberAccent, cardText: "Passed Text",),
-            MyCard(),
-          ],
-        ),
+        body: NestedScrollView(
+          headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
+            return <Widget>[
+              SliverAppBar(
+                expandedHeight: 200.0,
+                floating: false,
+                pinned: true,
+                flexibleSpace: FlexibleSpaceBar(
+                    centerTitle: true,
+                    title: Text("Collapsing Toolbar",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 16.0,
+                        )),
+                    background: Image.network(
+                      "https://images.pexels.com/photos/396547/pexels-photo-396547.jpeg?auto=compress&cs=tinysrgb&h=350",
+                      fit: BoxFit.cover,
+                    )),
+              ),
+            ];
+          },
+
+          body: Center(
+            child: MyCard(cardText: "Text", cardColor: Colors.blueGrey,),
+          ),
+
       ),
     );
   }
