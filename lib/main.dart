2246a297fe84fdc8da6dade30de3a09ac0b938e0
jesselima <jesselima.tech@gmail.com>
2019-12-22 07:20:19 -0300

    SilverBar and NestedScrollView With Tabs Sample 2

--- lib/main.dart
+++ lib/main.dart
@@ -29,10 +29,20 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
 
+  String sampleText = "Sample Text";
+
+
+
+  // ignore: slash_for_doc_comments
+  /**
+   * SAMPLE 2
+   */
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: NestedScrollView(
+      body: DefaultTabController(
+        length: 2,
+        child: NestedScrollView(
           headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
             return <Widget>[
               SliverAppBar(
@@ -51,14 +61,92 @@ class _MyHomePageState extends State<MyHomePage> {
                       fit: BoxFit.cover,
                     )),
               ),
+              SliverPersistentHeader(
+                delegate: _SliverAppBarDelegate(
+                  TabBar(
+                    labelColor: Colors.black87,
+                    unselectedLabelColor: Colors.grey,
+                    tabs: [
+                      Tab(
+                          icon: Icon(Icons.info),
+                          text: "Tab 1"
+                      ),
+                      Tab(
+                          icon: Icon(Icons.lightbulb_outline),
+                          text: "Tab 2"
+                      ),
+                    ],
+                  ),
+                ),
+                pinned: true,
+              ),
             ];
           },
-
           body: Center(
-            child: MyCard(cardText: "Text", cardColor: Colors.blueGrey,),
+            child: Text(sampleText),
           ),
-
+        ),
       ),
     );
   }
+}
+
+class _SliverAppBarDelegate extends SliverPersistentHeaderDelegate {
+  _SliverAppBarDelegate(this._tabBar);
+
+  final TabBar _tabBar;
+
+  @override
+  double get minExtent => _tabBar.preferredSize.height;
+  @override
+  double get maxExtent => _tabBar.preferredSize.height;
+
+  @override
+  Widget build(
+      BuildContext context, double shrinkOffset, bool overlapsContent) {
+    return new Container(
+      child: _tabBar,
+    );
+  }
+
+  @override
+  bool shouldRebuild(_SliverAppBarDelegate oldDelegate) {
+    return false;
+  }
+
+  /**
+   * SAMPLE 1
+   */
+//  @override
+//  Widget build(BuildContext context) {
+//    return Scaffold(
+//        body: NestedScrollView(
+//          headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
+//            return <Widget>[
+//              SliverAppBar(
+//                expandedHeight: 200.0,
+//                floating: false,
+//                pinned: true,
+//                flexibleSpace: FlexibleSpaceBar(
+//                    centerTitle: true,
+//                    title: Text("Collapsing Toolbar",
+//                        style: TextStyle(
+//                          color: Colors.white,
+//                          fontSize: 16.0,
+//                        )),
+//                    background: Image.network(
+//                      "https://images.pexels.com/photos/396547/pexels-photo-396547.jpeg?auto=compress&cs=tinysrgb&h=350",
+//                      fit: BoxFit.cover,
+//                    )),
+//              ),
+//            ];
+//          },
+//
+//          body: Center(
+//            child: MyCard(cardText: "Text", cardColor: Colors.blueGrey,),
+//          ),
+//
+//      ),
+//    );
+//  }
 }
